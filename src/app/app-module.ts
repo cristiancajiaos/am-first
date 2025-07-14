2479995339c232aa19d7cfe93fb58edc8ead7e7e
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-14 18:42:49 -0400

    Layout de la vista

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,17 +1,32 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCard, MatCardModule, MatCardTitle } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Layout } from './layout/layout';
+import { MatCardHeader } from "../../node_modules/@angular/material/card/index";
+import { MatChipsModule } from '@angular/material/chips';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Layout
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+],
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideClientHydration(withEventReplay())
